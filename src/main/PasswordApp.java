1e2f8c4621850c2ce85a4d87915a4aede109a4a7
sainiprathmesh <sainiprathmesh@gmail.com>
2020-01-09 00:00:57 +0530

    about to complete

---
+++ src/main/PasswordApp.java
@@ -0,0 +1,20 @@
+package main;
+
+
+import java.io.IOException;
+
+public class PasswordApp {
+    public static void main(String argv[]) {
+        char[] password = null;
+        try {
+            password = PasswordField.getPassword(System.in, "Enter your password: ");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        if (password == null) {
+            System.out.println("No password entered");
+        } else {
+            System.out.println("The password entered is: " + String.valueOf(password));
+        }
+    }
+}
